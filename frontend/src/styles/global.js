0dc279e426f4248fbee44e4d22d2ebb30ef187cf
Matheus <oliveira.msilva22@gmail.com>
2025-02-07 15:20:54 -0300

    Criando a tela de login e aplicando estilo

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,17 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0,
-        padding: 0,
-        box-sizing: border-box
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
     }
-    body {
-        width: 100vw;
-        height: 100vh;
-        background-color: #f0f2f5;
-        font-family: Arial, Helvetica, sans-serif;
-    }    
+    
+    html, body {
+        height: 100%;
+        margin: 0;
+        overflow: hidden; 
+    }
+    
+    #root {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+}
+  
 `;
 
 export default GlobalStyle;
